c4eec2bb1903ae75cacccc8052106e5470b1adfc
pangratz <pangratz666@gmail.com>
2011-08-29 15:47:21 +0200

    implemented ForumResource

--- src/main/java/com/pangratz/derstandardatcomments/rest/ForumResource.java
+++ src/main/java/com/pangratz/derstandardatcomments/rest/ForumResource.java
@@ -1,7 +1,42 @@
 package com.pangratz.derstandardatcomments.rest;
 
+import java.io.IOException;
+
+import org.restlet.data.MediaType;
+import org.restlet.data.Method;
+import org.restlet.ext.json.JsonRepresentation;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.representation.Variant;
+import org.restlet.resource.ResourceException;
 import org.restlet.resource.ServerResource;
+
+import com.pangratz.derstandardatcomments.model.Forum;
+import com.pangratz.derstandardatcomments.model.ForumFetcher;
 
 public class ForumResource extends ServerResource {
 
+	private ForumFetcher mForumFetcher;
+
+	@Override
+	protected void doInit() throws ResourceException {
+		super.doInit();
+
+		this.mForumFetcher = new ForumFetcher();
+
+		getVariants(Method.GET).add(new Variant(MediaType.APPLICATION_JSON));
+	}
+
+	@Override
+	protected Representation get(Variant variant) throws ResourceException {
+		// get the forum url
+		String url = getQuery().getFirstValue("url");
+
+		try {
+			Forum forum = mForumFetcher.getForum(url);
+			return new JsonRepresentation(forum.toJson());
+		} catch (IOException e) {
+			return new StringRepresentation("error: " + e.getMessage());
+		}
+	}
 }
